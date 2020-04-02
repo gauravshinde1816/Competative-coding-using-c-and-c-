af2adeafadf916ad7a1b3ee85862064cdc1b009d
gauravshinde1816 <gauravshinde1816@gmail.com>
2020-04-01 23:12:27 -0700

    fibonacci sequence using recursion

--- first.py
+++ first.py
@@ -1,10 +1,30 @@
-n =int(input("enter the number"))
-a,b=0,1
-sum=0
-for i in range(n):
-    if i<=1:
-        print(i , end=" ")
-    sum=a+b
-    a=b
-    b=sum
-    print(sum , end=" ")
+# a,b=0,1
+# sum=0
+# for i in range(n):
+#     if i<=1:
+#         print(i , end=" ")
+#     sum=a+b
+#     a=b
+#     b=sum
+#     print(sum , end=" ")
+
+#fibbonacci sequence using recursion#
+
+n = int(input("enter the number"))
+def fibbo(n):
+    if n<=1:
+       
+        return n
+
+    else:
+        return (fibbo(n-1)+fibbo(n-2))
+
+if n<=0:
+    print("enter the valid number")
+else:
+    print("Fibonacci sequence is :")
+    for i in range(n):
+        print(fibbo(i) , end=" ")
+
+
+        
