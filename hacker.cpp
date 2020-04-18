b7e057b880a4b24fd86db677d58cc4f766c46209
gauravshinde1816 <gauravshinde1816@gmail.com>
2020-04-17 19:45:18 -0700

    Call by reference

--- hacker.cpp
+++ hacker.cpp
@@ -1,35 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int  temp,min,loc;
-    // cout<<"enter the size of array";
-    
-    int A[10];
-    //cout<<"enter the array";
-    for(int i=0 ; i<4;i++){
-        cin>>A[i];
-    }
+void add(int *x, int *y){
+    int sum = *x + *y;
 
-for ( int i=0 ; i<3; i++){
+    int  Diff = abs(*x - *y);
 
-    min =A[i];
-    loc=i;
+    cout<<sum<<"\n";
 
-    for(int j=i+1; j<4;j++){
-        if(A[j]<min){
-            min=A[j];
-            loc=j;
-        }
-    }
-
-    temp=A[i];
-    A[i]=A[loc];
-    A[loc]=temp;
+    cout<<Diff;
 }
 
+int  main(){
 
+    int a,b;
 
-cout<<A[3];
+   
+
+    cin>>a>>b;
+
+    add(&a, &b);
 
 }
